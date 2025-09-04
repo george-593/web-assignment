b215d4a60b93ea146efd126821e72a6b22d84ff1
george-593 <63100333+george-593@users.noreply.github.com>
2025-09-05 00:35:33 +0100

    Old uncommitted code

--- js/header.js
+++ js/header.js
@@ -1,13 +1,13 @@
-const mobileMenuBtn = document.querySelector("#mobile-menu-btn")
-const mobileMenu = document.querySelector("#mobile-menu")
+const mobileMenuBtn = document.querySelector("#mobile-menu-btn");
+const mobileMenu = document.querySelector("#mobile-menu");
 
-classList = "bg-black w-[90%] py-8 mx-auto rounded-xl hidden"
+classList = "bg-black w-[90%] py-8 mx-auto rounded-xl hidden";
 mobileMenuBtn.addEventListener("click", () => {
-    if (mobileMenu.classList == classList) {
-        mobileMenu.classList.remove("hidden")
-        mobileMenuBtn.src = "/assets/icons/close.png"
-    } else {
-        mobileMenu.classList = classList
-        mobileMenuBtn.src = "/assets/icons/menu.png"
-    }
-})
+	if (mobileMenu.classList == classList) {
+		mobileMenu.classList.remove("hidden");
+		mobileMenuBtn.src = "./assets/icons/close.png";
+	} else {
+		mobileMenu.classList = classList;
+		mobileMenuBtn.src = "./assets/icons/menu.png";
+	}
+});
